cd98062acf1f8ffb6052d8354a72eeac523f6e46
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-04-26 18:39:41 -0400

    feat: font rework, packet info

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,9 @@ function App() {
   const [showTheme, setShowTheme] = useState<boolean>(false);
 
   const [showClues, setShowClues] = useState<boolean>(false);
+  const [packetType, setPacketType] = useState<boolean>(true); // intermediate: true; senior: false
   const [clueAmount, setClueAmount] = useState<number>(4);
+  const [round, setRound] = useState<number>(1);
   const clues = ["D", "C", "B", "A"];
 
   const eventListener = useShortcutEventListener("t", [showTheme], () => {
@@ -36,27 +38,36 @@ function App() {
     }
   );
 
+  const resetEventListener = useShortcutEventListener("0", [packetType, round], () => {
+    setRound(1);
+    setPacketType(true)
+  });
+
   useEffect(() => {
     document.addEventListener("keydown", eventListener);
     document.addEventListener("keydown", eventListener2);
     document.addEventListener("keydown", decrementListener);
+    document.addEventListener("keydown", resetEventListener);
 
     return () => {
       document.removeEventListener("keydown", eventListener);
       document.removeEventListener("keydown", eventListener2);
       document.removeEventListener("keydown", decrementListener);
+      document.removeEventListener("keydown", resetEventListener);
     };
   }, [eventListener, eventListener2]);
 
   return (
     <div className="App">
       <h1>Trivia Scoreboard</h1>
+      <div id='packetinfo' onClick={() => setPacketType(!packetType)}>Packet type: {(packetType) ? "Intermediate" : "Senior"}</div>
+      <div id='roundinfo' onClick={() => setRound(round + 1)}>Round {round}</div>
 
-      <Timer />
       <div id="wrapper">
         <Team teamNumber={1} />
         <Team teamNumber={2} />
       </div>
+      <Timer />
       <div id="theme" style={{ display: showTheme ? "block" : "none" }}>
         <h1>
           <button
