364b695e61ad32b160d22ff2d129db4f670ecd5f
Daniel Zhang <d.zhang200788@gmail.com>
2023-04-26 20:04:10 -0400

    audio starts from beginning every time

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -5,90 +5,90 @@ import { useShortcutEventListener, useAudio } from "../utils";
 // import { useReward } from "react-rewards";
 
 interface TeamProps {
-  teamNumber: number;
+    teamNumber: number;
 }
 
 const Team: FC<TeamProps> = ({ teamNumber }) => {
-  let choice =  Math.floor(Math.random() * 3);
-  console.log(choice)
-  // const { reward, isAnimating } = useReward(`reward${teamNumber}`, (choice === 0) ? "balloons" : "confetti", {
-  //   lifetime: 175,
-  //   elementCount: 20,
-  //   spread: 60,
-  // });
-  const [teamName, setTeamName] = useState<string>(
-    teamNumber === 1 ? "Team A" : "Team B"
-  );
-  const [score, setScore] = useState<number>(0);
-  const [scoreInput, setScoreInput] = useState<string>(score.toString());
-  const [focusState, setFocusState] = useState<boolean>(false);
-  const [playing, togglePlaying, audio] = useAudio("/Assets/ding.mp3");
+    let choice = Math.floor(Math.random() * 3);
+    console.log(choice);
+    // const { reward, isAnimating } = useReward(`reward${teamNumber}`, (choice === 0) ? "balloons" : "confetti", {
+    //   lifetime: 175,
+    //   elementCount: 20,
+    //   spread: 60,
+    // });
+    const [teamName, setTeamName] = useState<string>(
+        teamNumber === 1 ? "Team A" : "Team B"
+    );
+    const [score, setScore] = useState<number>(0);
+    const [scoreInput, setScoreInput] = useState<string>(score.toString());
+    const [focusState, setFocusState] = useState<boolean>(false);
+    const [playing, togglePlaying, audio] = useAudio("/Assets/ding.mp3");
 
-  const eventListener = useShortcutEventListener(
-    teamNumber.toString(),
-    [score, togglePlaying],
-    () => {
-      audio.play();
-      setScore(score + 10);
-      // reward();
-    }
-  );
-
-  const resetEventListener = useShortcutEventListener("0", [score], () => {
-    setScore(0);
-  });
-
-  useEffect(() => {
-    document.addEventListener("keydown", eventListener);
-    document.addEventListener("keydown", resetEventListener);
-
-    return () => {
-      document.removeEventListener("keydown", eventListener);
-      document.removeEventListener("keydown", resetEventListener);
-    };
-  }, [score, togglePlaying, eventListener, resetEventListener]);
-
-  return (
-    <div className="team">
-      <input
-        value={teamName}
-        onChange={(e) => setTeamName(e.target.value)}
-        className="teamName"
-      />
-      <input
-        value={focusState ? scoreInput : score}
-        onChange={(e) => setScoreInput(e.target.value)}
-        onFocus={() => {
-          setFocusState(true);
-          setScoreInput(score.toString());
-        }}
-        onBlur={() => {
-          setFocusState(false);
-          setScore(parseInt(scoreInput));
-        }}
-        className="score"
-      />
-      <div className="buttons">
-        <button
-          onClick={() => {
+    const eventListener = useShortcutEventListener(
+        teamNumber.toString(),
+        [score, togglePlaying],
+        () => {
+            new Audio("/Assets/ding.mp3").play();
             setScore(score + 10);
-            audio.play();
             // reward();
-          }}
-        >
-          <span id={`reward${teamNumber}`}>
-            <BsArrowUp />
-          </span>
-        </button>
-        <button onClick={() => setScore(score - 10)}>
-          <BsArrowDown />
-        </button>
-        <button onClick={() => setScore(0)}>
-          <AiOutlineReload />
-        </button>
-      </div>
-    </div>
-  );
+        }
+    );
+
+    const resetEventListener = useShortcutEventListener("0", [score], () => {
+        setScore(0);
+    });
+
+    useEffect(() => {
+        document.addEventListener("keydown", eventListener);
+        document.addEventListener("keydown", resetEventListener);
+
+        return () => {
+            document.removeEventListener("keydown", eventListener);
+            document.removeEventListener("keydown", resetEventListener);
+        };
+    }, [score, togglePlaying, eventListener, resetEventListener]);
+
+    return (
+        <div className="team">
+            <input
+                value={teamName}
+                onChange={(e) => setTeamName(e.target.value)}
+                className="teamName"
+            />
+            <input
+                value={focusState ? scoreInput : score}
+                onChange={(e) => setScoreInput(e.target.value)}
+                onFocus={() => {
+                    setFocusState(true);
+                    setScoreInput(score.toString());
+                }}
+                onBlur={() => {
+                    setFocusState(false);
+                    setScore(parseInt(scoreInput));
+                }}
+                className="score"
+            />
+            <div className="buttons">
+                <button
+                    onClick={() => {
+                        setScore(score + 10);
+                        audio.play();
+                        // reward();
+                    }}
+                >
+                    <span id={`reward${teamNumber}`}>
+                        <BsArrowUp />
+                    </span>
+                </button>
+                <button onClick={() => setScore(score - 10)}>
+                    <BsArrowDown />
+                </button>
+                <button onClick={() => setScore(0)}>
+                    <AiOutlineReload />
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Team;
